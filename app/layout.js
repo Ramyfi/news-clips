4f167ac256ea16529423f808598de3f459da8c66
Ramy <ramyfi.dev@gmail.com>
2024-04-29 16:51:21 -0400

    final changes with cleanup

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,28 @@
+// Layout component to structure the template of the App. Rameshwari Kothapalli, Natasha Machado.
 import "../globals.css";
-import Image from "next/image";
+import Image from "next/image"; // Logo of the NewsClips app
 
 export const metadata = {
   title: "News-Clips",
   description: "Summary of News Articles",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) { //Root Layout that defines the structure of the app.
   return (
     <html lang="en">
       <body>
-        <header>
+        {/* Header section with the App Logo */}
+        <header>    
           <Image
-            className="center"
+            className="center" 
             src="/Logo.png"
             width={70}
             height={70}
             alt="Logo"
           />
         </header>
-        {children}
+        {/* Child components or other pages are rendered here */}
+        {children} 
       </body>
     </html>
   );
